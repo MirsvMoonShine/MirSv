292ac3b43254d1ee90389ff1f308e264d87eb130
moonshine-PC\moonshine <os2402@naver.com>
2017-05-28 20:21:30 +0900

    Adjust enum PluginLists

--- src/com/mirsv/PluginLists.java
+++ src/com/mirsv/PluginLists.java
@@ -7,6 +7,10 @@ import com.mirsv.moonshine.GlobalMute.GlobalMute;
 import com.mirsv.moonshine.ItemTag.ItemTag;
 import com.mirsv.moonshine.Waring.Warning;
 import com.mirsv.moonshine.hungry.Hungry;
+import com.mirsv.moonshine.noblock.NoBlock;
+import com.mirsv.catnote.AdvanceClearEntity.*;
+import com.mirsv.moonshine.Welcome.*;
+import com.mirsv.catnote.CallPlayer.*;
 
 public enum PluginLists {	
 	GlobalMute("GlobalMute", new GlobalMute("GlobalMute")), 
@@ -15,11 +19,11 @@ public enum PluginLists {
 	Warning("Warning", new Warning("Warning")), 
 	BroadCast("BroadCast", new BroadCast("BroadCast")),
 	Hungry("Hungry", new Hungry("Hungry")),
-	NoBlock("NoBlock", new com.mirsv.moonshine.noblock.NoBlock("NoBlock")),
-	Welcome("Welcome", new com.mirsv.moonshine.Welcome.Welcome("Welcome")),
-	CallPlayer("CallPlayer",new com.mirsv.catnote.CallPlayer.CallPlayer("CallPlayer")),
+	NoBlock("NoBlock", new NoBlock("NoBlock")),
+	Welcome("Welcome", new Welcome("Welcome")),
+	CallPlayer("CallPlayer",new CallPlayer("CallPlayer")),
 	ControlDeathMessage("ControlDeathMessage", new ControlMessage("ControlDeathMessage")),
-	AdvanceClearEntity("AdvanceClearEntity", new com.mirsv.catnote.AdvanceClearEntity.AdvanceClearEntity("AdvanceClearEntity"));
+	AdvanceClearEntity("AdvanceClearEntity", new AdvanceClearEntity("AdvanceClearEntity"));
 	
 	private String PluginName;
 	private MirPlugin plugin;
