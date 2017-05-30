824cbfdf4ec1a8efe4bfab7bcaa6e8f72a23f7d6
MoonShine <os2402@naver.com>
2017-05-30 18:53:17 +0900

    GlobalMute 소스 줄임.

--- src/com/mirsv/moonshine/GlobalMute/GlobalMute.java
+++ src/com/mirsv/moonshine/GlobalMute/GlobalMute.java
@@ -1,25 +1,60 @@
 package com.mirsv.moonshine.GlobalMute;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.mirsv.MirPlugin;
 
-public class GlobalMute extends MirPlugin implements Listener{
+public class GlobalMute extends MirPlugin implements Listener, CommandExecutor {
+	public static boolean chat = true;
 
 	public GlobalMute() {
-		getCommand("gmute", new MuteCommand(getConfig()));
+		getCommand("gmute", this);
 		getListener(this);
 	}
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
-		if ((!MuteCommand.chat) && (getConfig().getBoolean("enable.GlobalMute", true))) {
+		if ((chat == false) && (getConfig().getBoolean("enable.GlobalMute", true))) {
 			if (e.getPlayer().hasPermission("mirsv.admin")) {
 				return;
 			}
 			e.setCancelled(true);
 		}
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
+		if (config.getBoolean("enable.GlobalMute", true)) {
+			if (!(sender instanceof Player)) {
+				if (chat) {
+					chat = false;
+					Bukkit.broadcastMessage("§a[미르서버] 전체 뮤트.");
+				} else {
+					chat = true;
+					Bukkit.broadcastMessage("§a[미르서버] 전체 뮤트 해제.");
+				}
+			} else {
+				Player p = (Player) sender;
+
+				if (!p.hasPermission("mirsv.admin")) {
+					p.sendMessage("§a[미르서버] §c펄미션 미 보유");
+				} else {
+					if (chat) {
+						chat = false;
+						Bukkit.broadcastMessage("§a[미르서버] 전체 뮤트.");
+					} else {
+						chat = true;
+						Bukkit.broadcastMessage("§a[미르서버] 전체 뮤트 해제.");
+					}
+				}
+			}
+		}
+		return true;
 	}
 }
