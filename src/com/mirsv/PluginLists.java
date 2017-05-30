2dcdd1f9a684a42a506f5901b410423f62272676
CatNote@LLr <CatNote1@gmail.com>
2017-05-30 21:13:26 +0900

    Edit Bugs

--- src/com/mirsv/PluginLists.java
+++ src/com/mirsv/PluginLists.java
@@ -9,21 +9,21 @@ import com.mirsv.moonshine.GlobalMute.GlobalMute;
 import com.mirsv.moonshine.ItemTag.ItemTag;
 import com.mirsv.moonshine.Warning.Warning;
 import com.mirsv.moonshine.hungry.Hungry;
-import com.mirsv.moonshine.Welcome.Welcome;
+// import com.mirsv.moonshine.Welcome.Welcome;
 import com.mirsv.moonshine.noblock.NoBlock;
 
 public enum PluginLists {
 	GlobalMute("GlobalMute", new GlobalMute()),
-		ClearChat("ClearChat", new ClearChat()),
-		ItemTag("ItemTag", new ItemTag()),
-		Warning("Warning", new Warning()),
-		BroadCast("BroadCast", new BroadCast()),
-		Hungry("Hungry", new Hungry()),
-		Welcome("Welcome", new Welcome()),
-		NoBlock("NoBlock", new NoBlock()),
-		CallPlayer("CallPlayer", new CallPlayer()),
-		ControlDeathMessage("ControlDeathMessage", new ControlMessage()),
-		WhisperChat("WhisperChat", new WhisperChat());
+	ClearChat("ClearChat", new ClearChat()),
+	ItemTag("ItemTag", new ItemTag()),
+	Warning("Warning", new Warning()),
+	BroadCast("BroadCast", new BroadCast()),
+	Hungry("Hungry", new Hungry()),
+	// Welcome("Welcome", new Welcome()),
+	NoBlock("NoBlock", new NoBlock()),
+	CallPlayer("CallPlayer", new CallPlayer()),
+	ControlDeathMessage("ControlDeathMessage", new ControlMessage()),
+	WhisperChat("WhisperChat", new WhisperChat());
 
 	private String PluginName;
 	private MirPlugin plugin;
