1065574c893c4d67f21dd53f8120102ef4b9a42a
CatNote@LLr <CatNote1@gmail.com>
2017-06-21 19:53:54 +0900

    걍 없앰

--- src/com/mirsv/catnote/PvPTeleport.java
+++ src/com/mirsv/catnote/PvPTeleport.java
@@ -1,6 +1,5 @@
 package com.mirsv.catnote;
 
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -12,11 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -31,54 +26,6 @@ public class PvPTeleport extends MirPlugin implements Listener, CommandExecutor 
 		getCommand("캠핑장", this);
 		getCommand("천공섬", this);
 		getListener(this);
-	}
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onCommand(PlayerCommandPreprocessEvent event) {
-		Player player = event.getPlayer();
-		if(player.isOp()) return;
-		String[] args = event.getMessage().split(" ");
-		final int ColoCX = 535, ColoCZ = 321, ColoRange = 50;
-		if(Math.sqrt(Math.pow(player.getLocation().getX() - ColoCX, 2) + Math.pow(player.getLocation().getZ() - ColoCZ, 2)) < ColoRange) {
-			if(!args[0].equalsIgnoreCase("/spawn") && !args[0].equalsIgnoreCase("/home") && !args[0].equalsIgnoreCase("/ts") && !(args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) return;
-			event.setCancelled(true);
-			if(args[0].equalsIgnoreCase("/ts") || (args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) {
-				Teleports.put(player.getUniqueId(), player.getLocation());
-				Times.put(player.getUniqueId(), new Date(System.currentTimeMillis()).getTime());
-			}
-			player.sendMessage(prefix + ChatColor.RED + "콜로세움에서는 이 명령어를 사용할 수 없습니다.");
-		}
-		final int SkyICX = 797, SkyICZ = 531, SkyIRange = 70;
-		if(Math.sqrt(Math.pow(player.getLocation().getX() - SkyICX, 2) + Math.pow(player.getLocation().getZ() - SkyICZ, 2)) < SkyIRange) {
-			if(!args[0].equalsIgnoreCase("/spawn") && !args[0].equalsIgnoreCase("/home") && !args[0].equalsIgnoreCase("/ts") && !(args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) return;
-			event.setCancelled(true);
-			if(args[0].equalsIgnoreCase("/ts") || (args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) {
-				Teleports.put(player.getUniqueId(), player.getLocation());
-				Times.put(player.getUniqueId(), new Date(System.currentTimeMillis()).getTime());
-			}
-			player.sendMessage(prefix + ChatColor.RED + "천공섬에서는 이 명령어를 사용할 수 없습니다.");
-		}
-		final int CampCX = 535, CampCZ = 510, CampRange = 70;
-		if(Math.sqrt(Math.pow(player.getLocation().getX() - CampCX, 2) + Math.pow(player.getLocation().getZ() - CampCZ, 2)) < CampRange) {
-			if(!args[0].equalsIgnoreCase("/spawn") && !args[0].equalsIgnoreCase("/home") && !args[0].equalsIgnoreCase("/ts") && !(args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) return;
-			event.setCancelled(true);
-			if(args[0].equalsIgnoreCase("/ts") || (args.length > 1 && args[0].equalsIgnoreCase("/t") && args[1].equalsIgnoreCase("spawn"))) {
-				Teleports.put(player.getUniqueId(), player.getLocation());
-				Times.put(player.getUniqueId(), new Date(System.currentTimeMillis()).getTime());
-			}
-			player.sendMessage(prefix + ChatColor.RED + "캠핑장에서는 이 명령어를 사용할 수 없습니다.");
-		}
-	}
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onCreatureSpawn(CreatureSpawnEvent event) {
-		if(!Teleports.isEmpty()) {
-			for(UUID uuid: Teleports.keySet()) {
-				long Now = new Date(System.currentTimeMillis()).getTime();
-				if(Bukkit.getOfflinePlayer(uuid).isOnline() && Now - Times.get(uuid) > 1000) {
-					Bukkit.getPlayer(uuid).teleport(Teleports.get(uuid));
-					Teleports.remove(uuid);
-				}
-			}
-		}
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
