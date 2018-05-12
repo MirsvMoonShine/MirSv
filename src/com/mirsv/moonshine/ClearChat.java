9b2870a3de754b930eec254cc9acb2ec1c978d5d
user <user@DESKTOP-GA4LC3D>
2018-05-12 15:48:36 +0900

    버그 수정

--- src/com/mirsv/moonshine/ClearChat.java
+++ src/com/mirsv/moonshine/ClearChat.java
@@ -18,11 +18,10 @@ public class ClearChat extends MirPlugin implements CommandExecutor{
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
 		if (((sender instanceof Player)) && (getConfig().getBoolean("enable.ClearChat", true))) {
-			Player p = (Player) sender;
 
-			if (p.hasPermission("mirsv.admin") || p.isOp()) {
+			if (sender.hasPermission("mirsv.admin") || sender.isOp()) {
 				for(int i = 0; i < 100; i++) for(Player p:Bukkit.getServer().getOnlinePlayers()) p.sendMessage("");
-				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + " &e" + p.getName() + "&f님이 채팅을 청소하셨습니다!"));
+				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + " &e" + sender.getName() + "&f님이 채팅을 청소하셨습니다!"));
 			}
 		}
 		return false;
